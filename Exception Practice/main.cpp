630cf2ad986604bf4059a15f1acab6247ddcf711
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-20 18:56:59 -0600

    custom exception

--- Exception Practice/main.cpp
+++ Exception Practice/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <exception>
 using namespace std;
+
+class CanGoWrong {
+public:
+	CanGoWrong() {
+		char* pMemory = new char[999];
+		delete[] pMemory;
+	};
+};
+
+class MyException : public exception {
+public:
+	virtual const char* what() const throw() {
+		return "Something bad happened!";
+	}
+};
+
+class Test {
+public:
+	void goesWrong() {
+		throw MyException();
+	}
+};
 
 void thisWillGoWrong() {
 	bool err1 = false;
 	bool err2 = false;
-	bool err3 = true;
+	bool err3 = false;
 
 	if (err1) {
 		throw 1;
@@ -22,9 +45,10 @@ void useThisWillGoWrong() {
 }
 
 int main() {
+	Test test;
 
 	try {
-		thisWillGoWrong();
+		test.goesWrong();
 	}
 	catch (int e) {
 		cout << "Error occured: " << e << endl;
@@ -32,10 +56,16 @@ int main() {
 	catch (const char* e) {
 		cout << "Const char pointer error: " << e << endl;
 	}
-	catch (string e) {
+	catch (string& e) {
 		cout << "String error: " << e << endl;
 	}
-
+	catch (bad_alloc& e) {
+		cout << "Caught exception: " << e.what() << endl;
+	}
+	catch (MyException& e) {
+		cout << e.what() << endl;
+	}
+	
 	cout << "Still running." << endl;
 
 
